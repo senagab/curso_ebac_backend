8be27ca5ddcde7d7a0ed5697e5409f3ce6cab5c3
senagab <senagab@gmail.com>
2023-09-24 12:27:56 -0400

    feat: melhoria em animações e inserção de imagens

--- main.js
+++ main.js
@@ -9,8 +9,22 @@ $(document).ready(function (){
     });
 
     $('form').on('submit', function (e) { // callback
-        console.log("submit");
+        
         e.preventDefault();
+        const enderecoNovaImagem = $('#endereco-imagem-nova').val();
+        console.log(enderecoNovaImagem);
+        const novoItem = $('<li style="display: none;"></li>');
+        $(`<img src="${enderecoNovaImagem}" />`).appendTo(novoItem);
+        $(`
+            <div class="overlay-imagem-link">
+                <a href="${enderecoNovaImagem}" target="_blank" title="ver imagem em tamanho real">
+                    ver imagem em tamanho real    
+                </a>
+            </div>
+        `).appendTo(novoItem);
+        $(novoItem).appendTo('ul');
+        $(novoItem).fadeIn('5000');
+        $('#endereco-imagem-nova').val(''); // reset input
     });
 
 });
