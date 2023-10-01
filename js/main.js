24026b604887e16f2257bf0b22cb246ad1b83493
senagab <senagab@gmail.com>
2023-10-01 13:13:47 -0400

    feat: adição de confirmação de envio

--- js/main.js
+++ js/main.js
@@ -4,4 +4,8 @@ $(document).ready(function () {
     $('#campo-cpf').mask('000.000.000-00');
 
     $('#campo-cep').mask('00000-000');
+
+    $('button').click(function () {
+        alert('formulário enviado com sucesso!')
+    })
 })
