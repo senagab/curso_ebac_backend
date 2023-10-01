277ab7a6b3b68e1ef4bd4229c4f8815d41787eac
senagab <senagab@gmail.com>
2023-10-01 12:56:36 -0400

    feat: estrutura html, jquery

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#campo-telefone').mask('(00) 00000-0000')
+
+    $('#campo-cpf').mask('000.000.000-00');
+
+    $('#campo-cep').mask('00000-000');
+})
--- metodo 2.js
+++
@@ -1,8 +0,0 @@
-function calculaArea(base, altura) {
-    base = prompt("insira o valor da base")
-    altura = prompt("insira o valor da altura")
-    let resultado = base * altura
-    console.log(resultado)
-}
-
-calculaArea() //callback?
